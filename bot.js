478dda45137bcea480622b7e1e92a62a38a5fe59
sinha104 <sinha104@umn.edu>
2020-06-30 22:36:31 -0500

    Remove console logs

--- bot.js
+++ bot.js
@@ -11,47 +11,32 @@ client.login(process.env.BOT_TOKEN);
 
 function buildNumericalList(dice_count, dice_size, add_sub = '+', increment_count = 0) {
     let currentList = [];
-    //let finalList = [];
     let sum = 0;
-
-    console.log(dice_count, dice_size, add_sub, increment_count);
 
     for (let i = 0; i < dice_count; i++) {
         let currentNum = parseInt(Math.random() * dice_size + 1);
-        //let finalNum = currentNum + (add_sub === '+' ? increment_count : -increment_count);
-        //// console.log(`${currentNum} + ${finalNum} : ${sum}`);
         currentList.push(currentNum);
-        //finalList.push(finalNum);
         sum += currentNum;
     }
-    //1console.log(sum);
-
     let addition_value = (add_sub === '+' ? increment_count : -increment_count);
 
-    //console.log(`Adding ${addition_value}`);
-
     sum += addition_value;
-    //console.log(sum);
-    // console.log(sum, currentList, finalList);
     return ([sum, currentList]);
 }
 
 client.on('message', msg => {
-    //// console.log(msg);
     if (/^!roll .*/.test(msg.content)) {
         const reg = /(?:(?<dice_count>\d+)[dD](?<dice_size>\d+))+(?: *(?<add_sub>[\+-]) *(?<constant>\d*))?\s*(?:#(?<comment>\w+))?/gm;
         let match, results = [];
         let sum = 0;
         let stringFin = '';
         let err_flag = false;
-        // console.log(msg.content);
         let lastComment = ""
         let stringToParse = msg.content;//.replace(" ", "");
 
         do {
             match = reg.exec(stringToParse);
-            console.log(match);
-            // // console.log(match && match !== null);
+            //console.log(match);
             if (match) {
                 let value = [];
 
