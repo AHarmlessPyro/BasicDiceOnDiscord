1a93ebc6db36803581ce539c05023bdb96ddf998
sinha104 <sinha104@umn.edu>
2020-06-30 20:23:43 -0500

    Remove all console messages

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    // console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(process.env.BOT_TOKEN);
@@ -14,33 +14,33 @@ function buildNumericalList(dice_count, dice_size, add_sub = '+', increment_coun
     let finalList = [];
     let sum = 0;
 
-    console.log(dice_count, dice_size, add_sub, increment_count);
+    // console.log(dice_count, dice_size, add_sub, increment_count);
 
     for (let i = 0; i < dice_count; i++) {
         let currentNum = parseInt(Math.random() * dice_size + 1);
         let finalNum = currentNum + (add_sub === '+' ? increment_count : -increment_count);
-        //console.log(`${currentNum} + ${finalNum} : ${sum}`);
+        //// console.log(`${currentNum} + ${finalNum} : ${sum}`);
         currentList.push(currentNum);
         finalList.push(finalNum);
         sum += finalNum;
     }
-    console.log(sum, currentList, finalList);
+    // console.log(sum, currentList, finalList);
     return ([sum, currentList, finalList]);
 }
 
 client.on('message', msg => {
-    //console.log(msg);
+    //// console.log(msg);
     if (/^!roll .*/.test(msg.content)) {
         const reg = /(?:(?<dice_count>\d+)[dD](?<dice_size>\d+))+(?: )?(?:(?<add_sub>[\+-]) *(?<constant>\d*))*/gm;
         let match, results = [];
         let sum = 0;
         let stringFin = '';
         let err_flag = false;
-        console.log(msg.content);
+        // console.log(msg.content);
         do {
             match = reg.exec(msg.content);
-            // console.log(match);
-            // console.log(match && match !== null);
+            // // console.log(match);
+            // // console.log(match && match !== null);
             if (match) {
                 results.push(match);
                 if (match.groups.add_sub) {
