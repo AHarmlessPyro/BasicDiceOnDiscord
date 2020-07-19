dc78becee4ad221a36d020beb3b91490383b8afb
sinha104 <sinha104@umn.edu>
2020-07-19 15:21:05 +0530

    Add ability to roll advantage/disadvantage in the roll20 format

--- bot.js
+++ bot.js
@@ -9,24 +9,75 @@ client.on('ready', () => {
 
 client.login(process.env.BOT_TOKEN);
 
-function buildNumericalList(dice_count, dice_size, add_sub = '+', increment_count = 0) {
+function buildNumericalList({ dice_count, dice_size, add_sub = '+', increment_count = 0, dice_to_keep = 'a', dice_number_to_keep = undefined }) {
     let currentList = [];
-    let sum = 0;
-
+    let sum = undefined;
+    let sortedList = [];
+    let total = {
+        List: [],
+        sum: 0
+    }
     for (let i = 0; i < dice_count; i++) {
         let currentNum = parseInt(Math.random() * dice_size + 1);
         currentList.push(currentNum);
-        sum += currentNum;
+        // sum += currentNum;
+    }
+
+    switch (dice_to_keep) {
+        case 'a':
+            sum = currentList.reduce((current) => {
+                total.List = [...total.List, current];
+                total.sum += current;
+            });
+            break;
+        case 'h':
+            if (dice_number_to_keep === undefined || dice_number_to_keep > currentList.length) {
+                dice_number_to_keep = currentList.length
+            }
+
+            sortedList = currentList.sort((a, b) => {
+                return -a + b;
+            })
+
+            sum = sortedList.forEach((currentValue, currentIndex) => {
+                if (currentIndex < dice_number_to_keep) {
+                    total.List = [...total.List, `${currentValue}`];
+                    total.sum += currentValue;
+                } else {
+                    total.List = [...total.List, `~~${currentValue}~~`];
+                }
+            })
+            break;
+        case 'l':
+            if (dice_number_to_keep === undefined || dice_number_to_keep > currentList.length) {
+                dice_number_to_keep = currentList.length
+            }
+
+            sortedList = currentList.sort((a, b) => {
+                return a - b;
+            })
+
+            sum = sortedList.forEach((currentValue, currentIndex) => {
+                if (currentIndex < dice_number_to_keep) {
+                    total.List = [...total.List, `${currentValue}`];
+                    total.sum += currentValue;
+                } else {
+                    total.List = [...total.List, `~~${currentValue}~~`];
+                }
+            })
+            break;
+        default:
+            break;
     }
     let addition_value = (add_sub === '+' ? increment_count : -increment_count);
 
-    sum += addition_value;
-    return ([sum, currentList, `d${dice_size}`]);
+    total.sum += addition_value;
+    return ([total.sum, total.List, `d${dice_size}`]);
 }
 
 client.on('message', msg => {
     if (/^!roll .*/.test(msg.content)) {
-        const reg = /(?:(?<dice_count>\d+)[dD](?<dice_size>\d+))+(?: *(?<add_sub>[\+-](?! ?\w+[dD]\w)) *(?<constant>\d*(?!d)))?\s*(?:#(?<comment>.+))?/gm;
+        const reg = /(?:(?<dice_count>\d+)[dD](?<dice_size>\d+)(?:[kK](?:(?<keep_H_or_L>[hlHL])(?<dice_to_keep>\d+)))?)+(?: *(?<add_sub>[\+-](?! ?\w+[dD]\w)) *(?<constant>\d*(?!d)))?\s*(?:#(?<comment>.+))?/gm;
         let match, results = [];
         let sum = 0;
         let stringFin = '';
@@ -42,16 +93,30 @@ client.on('message', msg => {
 
                 results.push(match);
 
+                if (match.groups.keep_H_or_L === undefined) {
+                    match.groups.keep_H_or_L = 'a';
+                    match.groups.dice_to_keep = undefined
+                } else {
+                    match.groups.dice_to_keep = parseInt(match.groups.dice_to_keep);
+                }
+
                 if (match.groups.add_sub) {
                     value = buildNumericalList(
-                        parseInt(match.groups.dice_count),
-                        parseInt(match.groups.dice_size),
-                        match.groups.add_sub,
-                        parseInt(match.groups.constant));
+                        {
+                            "dice_count": parseInt(match.groups.dice_count),
+                            "dice_size": parseInt(match.groups.dice_size),
+                            "add_sub": match.groups.add_sub,
+                            "increment_count": parseInt(match.groups.constant),
+                            "dice_to_keep": match.groups.keep_H_or_L,
+                            "dice_number_to_keep": match.groups.dice_to_keep
+                        });
                 } else {
-                    value = buildNumericalList(
-                        parseInt(match.groups.dice_count),
-                        parseInt(match.groups.dice_size));
+                    value = buildNumericalList({
+                        dice_count: parseInt(match.groups.dice_count),
+                        dice_size: parseInt(match.groups.dice_size),
+                        dice_to_keep: dice_to_keep = match.groups.keep_H_or_L,
+                        dice_number_to_keep: match.groups.dice_to_keep
+                    });
                 }
 
                 if (match.groups.comment) {
